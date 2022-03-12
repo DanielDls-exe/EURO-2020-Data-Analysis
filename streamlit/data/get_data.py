36816a7e7c35b5e3b054b35cd5ed911ec5af426d
DanielDls-exe <danieldls.ucv@gmail.com>
2022-03-11 21:27:15 -0400

    streamlit data change

--- streamlit/data/get_data.py
+++ streamlit/data/get_data.py
@@ -1,17 +1,20 @@
 import requests
-url = "https://127.0.0.1:8000"
+url = "http://127.0.0.1:8000"
 
 def get_all_players():
     return requests.get(url + "/players").json()
 
 def get_player(name):
-    return requests.get(url + "/player/{name}").json()
+    return requests.get(url + f"/player/{name}").json()
 
-def get_all_players():
+def get_all_name_players():
+    return requests.get(url + "/players/name/all").json()
+
+def get__all_teams():
     return requests.get(url + "/teams").json()
 
 def get_team(name):
-    return requests.get(url + "/team/{name}").json()
+    return requests.get(url + f"/team/{name}").json()
 
 def get_all_name_teams():
-    return requests.get(url + "/teams/name/all")
+    return requests.get(url + "/teams/name/all").json()
