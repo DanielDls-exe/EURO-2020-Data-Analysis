fd393bb8f8ee9a2e59c0970de8a85095af748403
DanielDls-exe <danieldls.ucv@gmail.com>
2022-03-14 08:39:57 -0400

    add endpoints

--- api/routers/players.py
+++ api/routers/players.py
@@ -1,6 +1,6 @@
 import json
 from fastapi import APIRouter
-from database.mongo import get_data
+from database.mongo import get_data, db
 from bson import json_util
 from json import loads
 
@@ -10,6 +10,33 @@ async def players_all():
     res = get_data("data_players")
     return loads(json_util.dumps(res))
 
+@router.get("/player/most")
+async def players_most_data(estadistic:str):
+    if estadistic == 'goals':
+        res = list(db["data_players"].find({}, {"_id":0, "name":1,"goals":1}).sort("goals", -1).limit(2))
+    elif estadistic == 'assist':
+        res = list(db["data_players"].find({}, {"_id":0, "name":1,"assistance":1}).sort("assistance", -1).limit(2))
+    else:
+        return {"mesagge": "That stadistic not exist"}
+    try:   
+        return loads(json_util.dumps(res[0]))
+    except:
+        return {"mesagge": "Oh, sorry, there's been a problem!"}
+    
+
+@router.get("/player/most/cards")
+async def players_data(color:str):
+    if color == 'yellow':
+        res = list(db["data_players"].find({}, {"_id":0, "name":1,"yellow_cards":1}).sort("yellow_cards", -1).limit(2))
+        return loads(json_util.dumps(res[0]))
+    elif color == 'red':
+        res = list(db["data_players"].find({}, {"_id":0, "name":1,"red_cards":1}).sort("red_cards", -1).limit(2))
+    try:   
+        return loads(json_util.dumps(res[0]))
+    except:
+        return {"mesagge": "Oh, sorry, there's been a problem!"}
+    
+
 @router.get("/player/{name}")
 async def players_data(name:str):
     res = get_data("data_players", {"name":name})
@@ -17,6 +44,8 @@ async def players_data(name:str):
         return loads(json_util.dumps(res[0]))
     except:
         return {"mesagge": "Oh, sorry, there's been a problem! That player does not exist"}
+
+
 
 @router.get("/player/{name}/goals")
 async def players_data(name:str):
