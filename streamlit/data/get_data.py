bbd5c1c1e28ca9e61a41f06e92259a541b56ed53
DanielDls-exe <danieldls.ucv@gmail.com>
2022-03-14 08:40:51 -0400

    add get data funtions

--- streamlit/data/get_data.py
+++ streamlit/data/get_data.py
@@ -10,6 +10,9 @@ def get_player(name):
 def get_all_name_players():
     return requests.get(url + "/players/name/all").json()
 
+def get_stadistic_player(stadistic):
+    return requests.get(url + "/player").json()
+
 def get__all_teams():
     return requests.get(url + "/teams").json()
 
@@ -18,3 +21,12 @@ def get_team(name):
 
 def get_all_name_teams():
     return requests.get(url + "/teams/name/all").json()
+
+def get_stadistic_team(stadistic):
+    return requests.get(url + f"/team/most?estadistic={stadistic}").json()
+
+def get_stadistic_player(stadistic):
+    return requests.get(url + f"/player/most?estadistic={stadistic}").json()
+
+def get_stadistic_player_cards(color):
+    return requests.get(url + f"/player/most/cards?color={color}").json()
